78c403a9b775a5630f60c51827200cb6b668f1d1
zcodlab <zmamanir@uni.edu.pe>
2026-06-10 09:40:30 -0500

    add componente persona y modelos

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RegistrarPersona } from './components/registrar-persona/registrar-persona';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RegistrarPersona],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })

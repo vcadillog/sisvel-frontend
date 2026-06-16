534c98b7a81cbfa0b40a1b7f9f80b3a443f6a765
zcodlab <zmamanir@uni.edu.pe>
2026-06-16 08:07:29 -0500

    add persona service

--- src/app/services/persona.service.ts
+++ src/app/services/persona.service.ts
@@ -1,6 +1,32 @@
-import { Injectable } from '@angular/core';
+import { Injectable,inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { PersonaRequest } from '../model/persona-request';
+import { PersonaResponse } from '../model/persona-response';
 
 @Injectable({
   providedIn: 'root',
 })
-export class PersonaService {}
+export class PersonaService {
+  private http=inject(HttpClient)
+
+  getPersona(): Observable<PersonaResponse[]>{
+    return this.http.get<PersonaResponse[]>(`${environment.url}/persona`)
+  }
+
+  registrarPersona(persona: PersonaRequest): Observable<PersonaResponse>{
+    return this.http.post<PersonaResponse>(`${environment.url}/persona`,persona)
+  }
+
+  actualizarPersona(persona: PersonaRequest): Observable<PersonaResponse>{
+    return this.http.put<PersonaResponse>(`${environment.url}/persona`,persona)
+  }
+
+  eliminarPersona(persona: PersonaRequest): Observable<PersonaResponse>{
+    return this.http.delete<PersonaResponse>(`${environment.url}/persona`,{
+      body:persona,
+    })
+  }
+
+}

4f77b198d60c9110e7208443597e445b778efa80
zcodlab <zmamanir@uni.edu.pe>
2026-06-16 07:57:48 -0500

    add ubigeo service

--- src/app/services/ubigeo.service.ts
+++ src/app/services/ubigeo.service.ts
@@ -1,6 +1,18 @@
-import { Injectable } from '@angular/core';
+import { Injectable,inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { Ubigeo } from '../model/ubigeo';
 
 @Injectable({
   providedIn: 'root',
 })
-export class UbigeoService {}
+export class UbigeoService {
+  private http=inject(HttpClient)
+
+  getUbigeo():Observable<Ubigeo[]>{
+      return this.http.get<Ubigeo[]>(`${environment.url}/ubigeo`);
+    }
+
+
+}
